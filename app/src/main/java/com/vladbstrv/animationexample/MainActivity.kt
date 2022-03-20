042baaefa6615e905ecb0bcc3702198bc6f13662
vlad-bstrv <keocera@icloud.com>
2022-03-20 15:47:47 +0300

    Познакомились с TransitionManager( Slide - выезжает)

--- app/src/main/java/com/vladbstrv/animationexample/MainActivity.kt
+++ app/src/main/java/com/vladbstrv/animationexample/MainActivity.kt
@@ -2,12 +2,10 @@ package com.vladbstrv.animationexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 
-import androidx.transition.TransitionManager
 import android.view.View
-import androidx.transition.ChangeBounds
-import androidx.transition.Fade
-import androidx.transition.TransitionSet
+import androidx.transition.*
 import com.vladbstrv.animationexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,11 +19,12 @@ class MainActivity : AppCompatActivity() {
 
         binding.button.setOnClickListener {
             val transition = TransitionSet()
-            val fade = Fade()
+            val fade = Slide()
+            fade.slideEdge = Gravity.START
             fade.duration = 5000
             val changeBounds = ChangeBounds()
             changeBounds.duration = 2000
-            transition.ordering = android.transition.TransitionSet.ORDERING_SEQUENTIAL
+            transition.ordering = android.transition.TransitionSet.ORDERING_TOGETHER
             transition.addTransition(changeBounds)
             transition.addTransition(fade)
             TransitionManager.beginDelayedTransition(binding.transitionContainer, transition)
