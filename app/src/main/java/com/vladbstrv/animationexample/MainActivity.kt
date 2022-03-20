7748d5cd8bd7bac9adb440d30c7ae011c97a4a59
vlad-bstrv <keocera@icloud.com>
2022-03-20 18:12:32 +0300

    Познакомились с ChangeImageTransform( изменяли размеры картинки)

--- app/src/main/java/com/vladbstrv/animationexample/MainActivity.kt
+++ app/src/main/java/com/vladbstrv/animationexample/MainActivity.kt
@@ -8,65 +8,30 @@ import android.view.LayoutInflater
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.*
 import com.vladbstrv.animationexample.databinding.ActivityMainBinding
 import com.vladbstrv.animationexample.databinding.ActivityMainExplodeBinding
+import com.vladbstrv.animationexample.databinding.ActivityMainZoomBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainExplodeBinding
+    lateinit var binding: ActivityMainZoomBinding
     private var flag = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainExplodeBinding.inflate(layoutInflater)
+        binding = ActivityMainZoomBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.recyclerView.adapter = Adapter()
-    }
+        binding.imageView.setOnClickListener {
 
-    private fun explode(clickedView: View){
-        val set = TransitionSet()
-        val explode = Explode()
-        val fade = Fade()
-        fade.duration = 5000
-        val rect = Rect()
-        clickedView.getGlobalVisibleRect(rect)
-        explode.duration = 2000
-        explode.excludeTarget(clickedView, true)
-        set.addTransition(explode)
-        set.addTransition(fade)
-        explode.epicenterCallback = object : Transition.EpicenterCallback() {
-            override fun onGetEpicenter(transition: Transition): Rect {
-                return rect
-            }
+            val changeBounds = ChangeImageTransform()
+            TransitionManager.beginDelayedTransition(binding.container, changeBounds)
 
-        }
-        TransitionManager.beginDelayedTransition(binding.recyclerView, set)
-        binding.recyclerView.adapter = null
-    }
-
-    inner class Adapter : RecyclerView.Adapter<ViewHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            return ViewHolder(
-                LayoutInflater.from(parent.context).inflate(
-                    R.layout.activity_main_explode_recycler_item,
-                    parent,
-                    false
-                ) as View
-            )
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.itemView.setOnClickListener {
-                explode(it)
-            }
-        }
-
-        override fun getItemCount(): Int {
-            return 32
+            flag = !flag
+            binding.imageView.scaleType = if(flag) ImageView.ScaleType.CENTER_CROP else ImageView.ScaleType.CENTER_INSIDE
         }
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 }
