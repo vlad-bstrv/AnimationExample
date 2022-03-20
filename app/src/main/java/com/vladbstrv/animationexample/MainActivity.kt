09cec0aafce8d3b2e66ce410601876ed87d89264
vlad-bstrv <keocera@icloud.com>
2022-03-20 18:27:33 +0300

    Задали путь(алгорит пути) для ChangeBounds

--- app/src/main/java/com/vladbstrv/animationexample/MainActivity.kt
+++ app/src/main/java/com/vladbstrv/animationexample/MainActivity.kt
@@ -8,29 +8,34 @@ import android.view.LayoutInflater
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.*
 import com.vladbstrv.animationexample.databinding.ActivityMainBinding
 import com.vladbstrv.animationexample.databinding.ActivityMainExplodeBinding
+import com.vladbstrv.animationexample.databinding.ActivityMainPathBinding
 import com.vladbstrv.animationexample.databinding.ActivityMainZoomBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainZoomBinding
+    lateinit var binding: ActivityMainPathBinding
     private var flag = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainZoomBinding.inflate(layoutInflater)
+        binding = ActivityMainPathBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.imageView.setOnClickListener {
+        binding.button.setOnClickListener {
 
-            val changeBounds = ChangeImageTransform()
-            TransitionManager.beginDelayedTransition(binding.container, changeBounds)
-
+            val changeBounds = ChangeBounds()
+            changeBounds.duration = 5000
+            changeBounds.setPathMotion(ArcMotion())
+            TransitionManager.beginDelayedTransition(binding.root, changeBounds)
             flag = !flag
-            binding.imageView.scaleType = if(flag) ImageView.ScaleType.CENTER_CROP else ImageView.ScaleType.CENTER_INSIDE
+            val params = binding.button.layoutParams as FrameLayout.LayoutParams
+            params.gravity = if(flag) Gravity.BOTTOM or Gravity.END else Gravity.START or Gravity.TOP
+            binding.button.layoutParams = params
         }
     }
 
